c2e62bb925a5911b1243354bafabc4c7ca1a6207
Nyerges Dominik <dommi7797@gmail.com>
2021-03-18 13:00:52 +0100

    added navbar needs design

--- Tournament Manager/ClientApp/src/App.js
+++ Tournament Manager/ClientApp/src/App.js
@@ -1,8 +1,54 @@
 import React from 'react';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
+import Home from './components/Home'
+import '../src/css/NavBar.css'
 
 function App() {
     return (
-        <p>Basic Init</p>
+            <div className="App">
+                <header className="App-header">
+                </header>
+        <Router>
+                <nav>
+                    <ul>
+                        <li className="NavElement">
+                            <Link to="/" className="NavElementLink">Home</Link>
+                        </li>
+                        <li className="NavElement">
+                            <Link to="/tournaments" className="NavElementLink">Tournaments</Link>
+                        </li>
+                        <li className="NavElement">
+                            <Link to="/leaderboard" className="NavElementLink">Leaderboard</Link>
+                        </li>
+                        <li className="NavElement">
+                            <Link to="/account" className="NavElementLink">Account</Link>
+                        </li>
+                    </ul>
+                </nav>
+                <Switch>
+                    <Route path="/">
+                        <Home />
+                    </Route>
+                    <Route path="/tournaments">
+                        <Home />
+                    </Route>
+                    <Route path="/leaderboard">
+                        <Home />
+                    </Route>
+                    <Route path="/login">
+                        <Home />
+                    </Route>
+                    <Route path="/register">
+                        <Home />
+                    </Route>
+                </Switch>
+                </Router>
+                </div>
     );
 }
 export default App;
---
+++ Tournament Manager/ClientApp/src/components/Home.js
@@ -0,0 +1,9 @@
+﻿import React from 'react';
+
+function Home(){
+    return (
+        <div>Works</div>
+    );
+}
+
+export default Home;
