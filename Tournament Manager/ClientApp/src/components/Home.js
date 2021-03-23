f8372783aa86eb55936e227f04732d5eeff7a002
Nyerges Dominik <dommi7797@gmail.com>
2021-03-23 11:34:13 +0100

    now displays join players

--- Tournament Manager/ClientApp/src/components/Home.js
+++ Tournament Manager/ClientApp/src/components/Home.js
@@ -6,10 +6,18 @@ function Home() {
 
     return (
         <div className="mainPage">
-            <p className="mainTitle">TOURNAMENT MANAGER</p>
-            <div className="linkContainer">
-                <Link to='/tournament-create' className="btnCreateTournament">Create Tournament</Link>
-                <Link to='/tournaments' className="btnCreateTournament">Join Tournament</Link>
+            <div className="top">
+                <p className="mainTitle">TOURNAMENT MANAGER</p>
+                <div className="linkContainer">
+                    <Link to='/tournament-create' className="btnCreateTournament">Create Tournament</Link>
+                    <Link to='/tournaments' className="btnCreateTournament">Join Tournament</Link>
+                </div>
+            </div>
+            <div className="middle">
+                I am Middle
+            </div>
+            <div className="bottom">
+                I am Bottom
             </div>
         </div>
     );
--- Tournament Manager/ClientApp/src/components/TournamentCard.js
+++ Tournament Manager/ClientApp/src/components/TournamentCard.js
@@ -3,9 +3,35 @@ import axios from 'axios';
 
 function TournamentCard(props) {
 
+    const [players, setPlayers] = useState([]);
+
+    useEffect(() => {
+        joinedPlayers();
+    }, []);
+
+    const id = props.tournament.id;
+
+    const cardStyling = {
+        width: "350px",
+        height: "215px",
+        display: "inline-block",
+        borderStyle: "solid",
+        borderRadius: "30px",
+        margin: "20px",
+        padding: "5px",
+        backgroundColor: "#2a2b2dff",
+        color: "white"
+    }
+
+    const joinedPlayers = () => {
+        axios.get('/contestants/' + id).then(data => setPlayers((data.data).length));
+    }
+
     return (
-        <div>
-            {props.tournament.name}
+        <div className="tournamentCard" style={cardStyling}>
+            <div style={{ height: "80px" }}>Tournament name: {props.tournament.name}</div>
+            <div style={{ height: "45px" }}><p>Tournament size: {props.tournament.size}/{players}</p></div>
+            <div style={{ height: "45px" }}><p>Applytime: {props.tournament.applyTime}</p></div>
         </div>
     );
 }
