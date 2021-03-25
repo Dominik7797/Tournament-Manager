adb5b4b39e74af784e6e26370ce24014a479fddf
Nyerges Dominik <dommi7797@gmail.com>
2021-03-25 14:02:58 +0100

    added img based on tournament type

--- Tournament Manager/ClientApp/src/components/TournamentCard.js
+++ Tournament Manager/ClientApp/src/components/TournamentCard.js
@@ -1,13 +1,16 @@
 ﻿import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import '../css/TournamentCardCSS.css'
+import TennisImage from "../images/Tennis.jpg";
 
 function TournamentCard(props) {
 
     const [players, setPlayers] = useState([]);
+    const [typeImg, setTypeImg] = useState([]);
 
     useEffect(() => {
         joinedPlayers();
+        getTournamentPicture();
     }, []);
 
     const id = props.tournament.id;
@@ -16,12 +19,19 @@ function TournamentCard(props) {
         axios.get('/contestants/' + id).then(data => setPlayers((data.data).length));
     }
 
+    const getTournamentPicture = () => {
+        if (props.tournament.tournamentType == "Tennis") {
+            setTypeImg(TennisImage);
+        }
+    }
+
     return (
+        console.log(typeImg),
         <div class="tournaments-container">
             <div class="tournament">
-                <div class="tournament-preview">
+                <div class="tournament-preview" style={{ backgroundImage: `url(${typeImg})`}}>
                     <h6>Tournament type:</h6>
-                    <h2>JavaScript Fundamentals</h2>
+                    <h2>{props.tournament.tournamentType}</h2>
                 </div>
                 <div class="tournament-info">
                     <div class="tournament-container">
--- Tournament Manager/Controllers/TournamentController.cs
+++ Tournament Manager/Controllers/TournamentController.cs
@@ -29,11 +29,11 @@ namespace Tournament_Manager.Controllers
         [HttpPost("/Tournament-create/tournamentName={tName}&tsize={tSize}&ttsize={tTSize}&password={password}&applytime={applyTime}&type={type}")]
         public StatusCodeResult CreateTournament(string tname, string tSize, string tTSize, string password, string applyTime, string type)
         {
+            Console.WriteLine(tname);
+
             try
             {
-                var time = applyTime.Split('-');
-                var dateTime = new DateTime(int.Parse(time[0]), int.Parse(time[1]), int.Parse(time[2]));
-                Tournament tournament = new Tournament(tname, int.Parse(tSize), int.Parse(tTSize), password, dateTime, type);
+                Tournament tournament = new Tournament(tname, int.Parse(tSize), int.Parse(tTSize), password, DateTime.Parse(applyTime), type);
                 _tournamentRepository.Add(tournament);
                 return StatusCode(204);
             }
