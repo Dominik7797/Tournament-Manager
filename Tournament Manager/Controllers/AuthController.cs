77f3ea4b83ec09fc2d4b7ff219ce29ad28d99dcb
Nyerges Dominik <dommi7797@gmail.com>
2021-03-30 11:32:53 +0200

    user password is now hashed and saved to the db.

--- Tournament Manager/Controllers/AuthController.cs
+++ Tournament Manager/Controllers/AuthController.cs
@@ -4,18 +4,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Tournament_Manager.Models;
+using Tournament_Manager.Services;
 
 namespace Tournament_Manager.Controllers
 {
     [Route("/auth")]
     [ApiController]
-    public class AuthController
+    public class AuthController : ControllerBase
     {
         private readonly IUsersRepository _usersRepository;
+        private readonly IHashService _hashService;
 
-        public AuthController(IUsersRepository usersRepository)
+        public AuthController(IUsersRepository usersRepository, IHashService hashService)
         {
             _usersRepository = usersRepository;
+            _hashService = hashService;
         }
 
         [HttpGet("/verify/username={username}&email={email}&password={password}")]
@@ -23,7 +26,8 @@ namespace Tournament_Manager.Controllers
         {
             if (_usersRepository.VerifyUser(username, email))
             {
-                _usersRepository.Add(new User(username, email, password));
+                var hashPassword = _hashService.Hash(password);
+                _usersRepository.Add(new User(username, email, hashPassword));
             }
         }
     }
