122dce0537695ffa2ec72e3aa646c875fd6ac463
Nyerges Dominik <dommi7797@gmail.com>
2021-05-05 15:45:14 +0200

    added transperent nav needs data

--- Tournament Manager/ClientApp/src/components/TournamentNavbar.js
+++ Tournament Manager/ClientApp/src/components/TournamentNavbar.js
@@ -4,34 +4,24 @@ import Navbar from 'react-bootstrap/Navbar';
 import {
     Link
 } from "react-router-dom";
-import '../css/NavbarCSS.css'
 
 
 export default function TournamentNavbar() {
     return (
-        <nav class="navbar navbar sticky-top navbar-toggler navbar-dark bg-dark navbar-expand-custom">
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <Navbar.Brand as={Link} to="/">Tournament-Manager</Navbar.Brand>
-                    <Nav className="mr-auto">
-                        <Nav.Link as={Link} to="/tournaments">Tournaments</Nav.Link>
-                        <Nav.Link as={Link} to="/tournament-create">Creator</Nav.Link>
-                        <Nav.Link as={Link} to="/leaderboard">Leaderboard</Nav.Link>
-                    </Nav>
-                    <Nav>
-                        <li class="nav-item dropdown">
-                        <Nav.Link as={Link} class="nav-link dropdown-toggle nav nav-item " to="/account" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            Account
-                        </Nav.Link>
-                            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                <Nav.Link as={Link} class="dropdown-item" to="/login">Sign In</Nav.Link>
-                                <Nav.Link as={Link} class="dropdown-item" to="/register">Registration</Nav.Link>
-                            </ul>
-                        </li>
-                    </Nav>
+        <nav class="navbar navbar-expand-lg fixed-top py-3">
+            <div class="container"><a href="#" class="navbar-brand text-uppercase font-weight-bold">Transparent Nav</a>
+                <button type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation" class="navbar-toggler navbar-toggler-right"><i class="fa fa-bars"></i></button>
+
+                <div id="navbarSupportedContent" class="collapse navbar-collapse">
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item active"><a href="#" class="nav-link text-uppercase font-weight-bold">Home <span class="sr-only">(current)</span></a></li>
+                        <li class="nav-item"><a href="#" class="nav-link text-uppercase font-weight-bold">About</a></li>
+                        <li class="nav-item"><a href="#" class="nav-link text-uppercase font-weight-bold">Gallery</a></li>
+                        <li class="nav-item"><a href="#" class="nav-link text-uppercase font-weight-bold">Portfolio</a></li>
+                        <li class="nav-item"><a href="#" class="nav-link text-uppercase font-weight-bold">Contact</a></li>
+                    </ul>
                 </div>
-</nav>
+            </div>
+        </nav>
     );
 }
